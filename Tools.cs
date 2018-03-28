945c31f659dbbf642d353a3b76809ab644b6e623
zm <240877724qq.com>
2018-03-28 15:11:39 +0800

    这三次

--- Tools.cs
+++ Tools.cs
@@ -10,3 +10,10 @@ Untracked files:
 Untracked files:
 Untracked files:
 Untracked files:
+
+git commit
+git commit --amend，修补式提交。
+git commit --a，对本地所有变更的文件执行提交操作，包括对本地修改的文件和删除的文件，但是不包括未被版本库跟踪的文件。但是这个命令最好不要使用，这样会丢掉Git暂存区带给用户的最大好处：对提交内容进行控制的能力
+git commit --allow-empty，允许执行空白提交
+
+
